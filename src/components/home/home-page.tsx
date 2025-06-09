7c614bc2cb1dc10bcbd5aed59e3dc08a3b505747
Ignacio Lopez Coll <joseignaciolopezcoll@gmail.com>
2025-06-09 17:04:14 -0600

    Change cron job

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 import "../../styles/home-page.css"
 import { LocalizationBanner } from "@/components/home/header/localization-banner"
 import { HeroSection } from "@/components/home/hero-section/hero-section"
-import { Pricing } from "@/components/home/pricing/pricing"
+
 import { HomePageBackground } from "@/components/gradients/home-page-background"
 import { Footer } from "@/components/home/footer/footer"
 
@@ -17,7 +17,7 @@ export function HomePage() {
       <div>
         <HomePageBackground />
         <HeroSection />
-        <Pricing country={country} />
+
         <Footer />
       </div>
     </>
